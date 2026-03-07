e9e0c541b5122cca840cfa749e772d4266f2f15d
Al McNicoll <github@almcnicoll.co.uk>
2026-03-07 23:05:04 +0000

    Got piggy-bank working

--- resources/views/picks/today.blade.php
+++ resources/views/picks/today.blade.php
@@ -74,12 +74,10 @@
             </div>
             <div class="circles-add-col">
                 <a href="{{ route('causes.create') }}" class="add-cause-btn" title="Add new cause">+</a>
-                <svg id="piggy-bank"
-                    style="width:120px; margin-top:1rem; display:block;"
-                    role="img"
-                    aria-label="Piggy bank">
-                    <use href="{{ asset('images/piggy-bank.svg') }}#svgRoot"></use>
-                </svg>
+                <img id="piggy-bank"
+                    src="http://mite.test/images/piggy-bank.svg"
+                    alt="Piggy bank"
+                    style="width:120px; margin-top:1rem; display:block;">
             </div>
         </div>
 
@@ -219,7 +217,9 @@
     /* ── Add button ── */
     .circles-add-col {
         display: flex;
+        flex-direction: column;
         align-items: center;
+        gap: 1rem;
     }
 
     .add-cause-btn {
